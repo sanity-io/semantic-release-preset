7c3ebaf1b9c0db79b459ecd962963004a80cd619
Cody Olsen <stipsan@gmail.com>
2022-08-16 23:14:51 +0200

    chore: test ci

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('TODO')
+// @ts-check
+
+/**
+ * @type {import('semantic-release').Options}
+ **/
+const options = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'release',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'release/*.tgz',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
+}
+
+module.exports = options
